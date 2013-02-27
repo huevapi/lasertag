794d43c4e082833b15a0c370cc14787ea63496a4
Ignacio Baixas <iobaixas@gmai.com>
2013-02-27 20:54:27 -0300

    adds static file support

--- site/server.js
+++ site/server.js
@@ -1,39 +1,15 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').createServer(app);
 var webRTC = require('webrtc.io').listen(server);
 
 server.listen(8000);
 
-
+app.use(express.static(__dirname + '/'));
 
 app.get('/', function(req, res) {
   res.sendfile(__dirname + '/index.html');
 });
-
-app.get('/style.css', function(req, res) {
-  res.sendfile(__dirname + '/style.css');
-});
-
-app.get('/fullscrean.png', function(req, res) {
-  res.sendfile(__dirname + '/fullscrean.png');
-});
-
-app.get('/script.js', function(req, res) {
-  res.sendfile(__dirname + '/script.js');
-});
-
-app.get('/hub.js', function(req, res) {
-  res.sendfile(__dirname + '/hub.js');
-});
-
-app.get('/client.js', function(req, res) {
-  res.sendfile(__dirname + '/client.js');
-});
-
-app.get('/webrtc.io.js', function(req, res) {
-  res.sendfile(__dirname + '/webrtc.io.js');
-});
-
 
 webRTC.rtc.on('connect', function(rtc) {
   //Client connected
